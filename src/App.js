f387aa667db1aa957bae4788f689e32b615a5096
yardiewizardie <yardiewizardie@gmail.com>
2018-01-01 17:49:36 -0430

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         App Header
+          <h1 className="App-title">App Title</h1>
+        </header>
+        <p className="App-intro">
+          xxxx
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
