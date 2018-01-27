a8c073048cac16800d2a732c60aeb6cf5d51142f
yardiewizardie <yardiewizardie@gmail.com>
2018-01-26 22:37:53 -0430

    refactor the newpost functions

--- src/components/newpost.js
+++ src/components/newpost.js
@@ -1,8 +1,15 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { CreatePosts } from '../actions';
+
+const FIELDS = {
+	title: { type: 'input', label: 'Enter some title' },
+	categories: { type: 'input', label: 'Enter some categories' },
+	content: { type: 'textarea', label: 'Enter some description' },
+};
 
 class NewPost extends Component {
 	renderField(field) {
@@ -10,29 +17,17 @@ class NewPost extends Component {
 			<div className="cont container-fluid">
 				<label className="newpostfont">{field.label}</label>
 				<div className="form-group has-danger">
-					<input className="inputstyle form-control  input-sm" type="text" {...field.input} />
-				</div>
-
-				<div className="text-help">{field.meta.touched ? field.meta.error : ''}</div>
-			</div>
-		);
-	}
-
-	renderFieldDesc(field) {
-		return (
-			<div className="container cont container-fluid">
-				<label className="newpostfont">{field.label}</label>
-				<div className="form-group has-danger">
-					<textarea
-						className="inputstyle form-control"
-						type="text"
+					<field.type
+						className="inputstyle form-control  input-sm"
 						rows="8"
 						cols="50"
 						name="content"
 						form="usrform"
+						type="text"
 						{...field.input}
 					/>
 				</div>
+
 				<div className="text-help">{field.meta.touched ? field.meta.error : ''}</div>
 			</div>
 		);
@@ -52,15 +47,15 @@ class NewPost extends Component {
 					<div className="row">
 						<div className="col-md-1" />
 						<div className="col-md-5 contnewpage">
-							<Field label="Title" name="title" component={this.renderField} />
+							<Field label="Title" name="title" type="input" component={this.renderField} />
 						</div>
 
 						<div className="col-md-5 contnewpage">
-							<Field label="Categories" name="categories" component={this.renderField} />
+							<Field label="Categories" name="categories" type="input" component={this.renderField} />
 						</div>
 						<div className="col-md-1" />
 					</div>
-					<Field label="Type your dookie!" name="content" component={this.renderFieldDesc} />
+					<Field label="Type your dookie!" name="content" type="textarea" component={this.renderField} />
 					<div className="col-md-6">
 						<button type="submit" className=" btn-danger edit-btn leftbtn container-fluid">
 							<i className="fa fa-floppy-o fa-3x" aria-hidden="true" />
@@ -80,22 +75,16 @@ class NewPost extends Component {
 function validate(values) {
 	const errors = {};
 
-	if (!values.title) {
-		errors.title = 'Enter a Title';
-	}
-
-	if (!values.categories) {
-		errors.categories = 'Enter 1 or more categories';
-	}
-
-	if (!values.content) {
-		errors.content = 'Enter a description';
-	}
-
+	_.each(FIELDS, (type, field) => {
+		if (!values[field]) {
+			errors[field] = `Error enter some ${field}`;
+		}
+	});
 	return errors;
 }
 
 export default reduxForm({
 	validate,
 	form: 'PostNewForm',
+	fields: _.keys(FIELDS),
 })(connect(null, { CreatePosts })(NewPost));
