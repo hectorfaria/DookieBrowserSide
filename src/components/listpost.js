25af8ccbd5c2dccf49446797d3ec8972f4c20109
yardiewizardie <yardiewizardie@gmail.com>
2018-01-30 12:02:46 -0430

    styling v2

--- src/components/listpost.js
+++ src/components/listpost.js
@@ -42,7 +42,8 @@ class ListPosts extends Component {
 		return (
 			<div>
 				<Helmet>
-					<style>{'body { background: url("https://cdn-images-1.medium.com/max/2000/1*7v_75ZGg1CTmWAw1rEgMHQ.jpeg"); }'}</style>
+					<style>{'body { background: url("https://cdn-images-1.medium.com/max/2000/1*7v_75ZGg1CTmWAw1rEgMHQ.jpeg");'}</style>
+					<style>{'body { background-size: cover'}</style>
 				</Helmet>
 				<div className="listp">
 					<button className=" btn-danger edit-btn listsp container-fluid" onClick={this.onOpenModal}>
